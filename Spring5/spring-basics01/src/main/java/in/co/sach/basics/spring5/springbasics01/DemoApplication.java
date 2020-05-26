9420a8a7241752f323a14c956b817840d2420f35
Sandeep Chandragiri <sandeep_chandragiri@thbs.com>
2020-05-26 13:06:31 +0530

    Inital commit

---
+++ Spring5/spring-basics01/src/main/java/in/co/sach/basics/spring5/springbasics01/DemoApplication.java
@@ -0,0 +1,14 @@
+package in.co.sach.basics.spring5.springbasics01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("IN DEMO");
+	}
+
+}
---
+++ Spring5/spring-basics01/src/test/java/in/co/sach/basics/spring5/springbasics01/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.sach.basics.spring5.springbasics01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
